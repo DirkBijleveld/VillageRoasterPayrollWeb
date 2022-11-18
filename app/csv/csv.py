be09d8c2652c401aaa8e8a3ce5c56f143f74598f
sheepination <dirkwbijleveld@gmail.com>
2022-11-18 01:17:00 +0100

    Expanded csv cleaning abilities. Serious improvements can be made in simplifying data-type coercion. This will be fixed for the next version.
    Tests do not pass due to .dtype assertions not doing what I want them to.

--- app/csv/csv.py
+++ app/csv/csv.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from decimal import Decimal
 from typing import BinaryIO
 
 import pandas as pd
@@ -43,4 +44,181 @@ def clean_blanks(df: pd.DataFrame) -> pd.DataFrame:
         & ~(df[first_col] == "-")
         & ~(df[first_col].str.startswith("Totals for "))
     ]
+    return df
+
+
+@cleaner
+def clean_excess_headers(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Removes duplicate header rows buried in .csv
+    """
+    first_col = df.columns[0]
+    df = df[~(df[first_col] == first_col)]
+    return df
+
+
+@cleaner
+def clean_empty_shifts(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Removes shifts without a clock in time AND clock out time, AND removes shifts without a clock in date AND clock out date
+    """
+    df = df[
+        ~(pd.isna(df["Clock in time"]) & pd.isna(df["Clock out time"]))
+        & ~(pd.isna(df["Clock in date"]) & pd.isna(df["Clock out date"]))
+    ]
+    return df
+
+
+@cleaner
+def clean_types(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Forces all columns to be the correct type.
+    This is the most fundamentally complex and important part of the cleaning process.
+    This has been the source of 99% of bugs with the software...
+    """
+    # NAME
+    # Fill empty (NaN) values with empty strings (otherwise forcing type causes an error)
+    df["Name"] = df["Name"].fillna("")
+    # Force type to string
+    df["Name"] = df["Name"].astype(str)
+    # Remove leading and trailing whitespace
+    df["Name"] = df["Name"].str.strip()
+
+    # PAYROLL ID
+    # Fill empty (NaN) values with 9999 (otherwise forcing type causes an error)
+    df["Payroll ID"] = df["Payroll ID"].fillna(9999)
+    # Force type to integer
+
+    def payroll_id_to_int(payroll_id) -> int:
+        """
+        Converts a payroll ID to an integer.
+        Local function because it's only used in this one place.
+        try: except: is usually bad practice, but this piece is integral to functionality.
+        Finally, any problems with the Payroll ID can and will be resolved manually later,
+        so the cost of missing one minor rare exception is worth the cost of not crashing.
+        """
+        try:
+            return int(payroll_id)
+        except ValueError:
+            return 9999
+
+    df["Payroll ID"] = df["Payroll ID"].apply(payroll_id_to_int)
+
+    # CLOCK IN DATETIME
+
+    def as_datetime(s: str, can_null: bool = True) -> datetime | None:
+        """
+        Converts a date and time to a datetime object.
+        Local function because it's only used in this one place.
+        """
+        try:
+            return datetime.strptime(f"{s}", "%d %B %Y%I:%M%p")
+        except ValueError:
+            if can_null:
+                return None
+            raise ValueError(f"Failed to construct datetime out of input {s}")
+
+    df["Clock in datetime"] = df["Clock in date"] + df["Clock in time"]
+    df["Clock in datetime"] = df["Clock in datetime"].apply(as_datetime)
+
+    # CLOCK OUT DATETIME
+    df["Clock out datetime"] = df["Clock out date"] + df["Clock out time"]
+    df["Clock out datetime"] = df["Clock out datetime"].apply(as_datetime)
+
+    # BREAK START
+    df["Break start"] = df["Clock in date"] + df["Break start"]
+    df["Break start"] = df["Break start"].apply(as_datetime)
+
+    # BREAK END
+    df["Break end"] = df["Clock in date"] + df["Break end"]
+    df["Break end"] = df["Break end"].apply(as_datetime)
+
+    # ROLE
+    df["Role"] = df["Role"].fillna("None")
+    df["Role"] = df["Role"].astype(str)
+
+    # WAGE
+    df["Wage"] = df["Wage"].fillna("0")
+
+    def wage_fix(wage) -> str:
+        try:
+            return str(wage)
+        except ValueError:
+            return "0"
+
+    df["Wage"] = df["Wage"].apply(wage_fix)
+    df["Wage"] = df["Wage"].apply(lambda wage: Decimal(wage.strip("$").strip(",")))
+
+    def wage_int(wage: Decimal) -> int:
+        return int(wage * 1000)
+
+    df["Wage"] = df["Wage"].apply(wage_int)
+
+    # SCHEDULED HOURS
+    df["Scheduled"] = df["Scheduled"].fillna("0")
+
+    def scheduled_fix(scheduled) -> str:
+        try:
+            return str(scheduled)
+        except ValueError:
+            return "0"
+
+    df["Scheduled"] = df["Scheduled"].apply(scheduled_fix)
+    df["Scheduled"] = df["Scheduled"].apply(lambda scheduled: Decimal(scheduled.strip("$").strip(",")))
+
+    def scheduled_int(scheduled: Decimal) -> int:
+        return int(scheduled * 1000)
+
+    df["Scheduled"] = df["Scheduled"].apply(scheduled_int)
+
+    # ISSUES COLUMN
+    if "Issues" not in df:
+        df["Issues"] = ""
+    df["Issues"] = df["Issues"].fillna("")
+
+    def issues_fix(issues) -> str:
+        try:
+            return str(issues)
+        except ValueError:
+            return ""
+
+    df["Issues"] = df["Issues"].apply(issues_fix)
+
+    # EMPLOYEE NOTE COLUMN
+    if "Employee note" not in df:
+        df["Employee note"] = ""
+    df["Employee note"] = df["Employee note"].fillna("")
+
+    def employee_note_fix(employee_note) -> str:
+        try:
+            return str(employee_note)
+        except ValueError:
+            return ""
+
+    df["Employee note"] = df["Employee note"].apply(employee_note_fix)
+
+    # MANAGER NOTE COLUMN
+    if "Manager note" not in df:
+        df["Manager note"] = ""
+
+    df["Manager note"] = df["Manager note"].fillna("")
+
+    def manager_note_fix(manager_note) -> str:
+        try:
+            return str(manager_note)
+        except ValueError:
+            return ""
+
+    df["Manager note"] = df["Manager note"].apply(manager_note_fix)
+
+    # BREAK PAID BOOLEAN
+    def break_paid_fix(break_paid) -> bool:
+        if not isinstance(break_paid, str):
+            return False
+        if break_paid == "30 min - Paid":
+            return True
+        return False
+
+    df["Break paid"] = df["Break type"].apply(break_paid_fix)
+
     return df
--- app/tests/test_csv.py
+++ app/tests/test_csv.py
@@ -1,7 +1,19 @@
 from pathlib import Path
 
 import pandas as pd
+from pandas import read_csv
 from pytest import fixture
+from pytest import mark
+
+from app.csv.csv import (
+    get_payroll_period,
+    read_file,
+    clean_blanks,
+    clean_excess_headers,
+    clean_empty_shifts, clean_types,
+)
+
+param = mark.parametrize
 
 
 @fixture
@@ -25,32 +37,81 @@ def csv_2_path(csv_dir: Path) -> Path:
 
 
 @fixture
-def df_fake() -> pd.DataFrame:
-    return pd.DataFrame(
-        {
-            "Name": ["John", "Jane", "Joe"],
-            "Clock in date": ["2021-01-01", "2021-01-01", "2021-01-01"],
-            "Clock in time": ["8:00am", "8:00am", "8:00am"],
-            "Clock out date": ["2021-01-01", "2021-01-01", "2021-01-01"],
-            "Clock out time": ["4:00pm", "4:00pm", "4:00pm"],
-            "Break start": ["", "", ""],
-            "Break end": ["", "", ""],
-            "Break length": ["", "", ""],
-            "Break type": ["", "", ""],
-            "Payroll ID": [1, 2, 3],
-            "Role": ["Admin", "Main Store", "Hospital"],
-            "Wage": ["$16.00", "$16.00", "$16.00"],
-            "Scheduled": ["", "", ""],
-            "Actual vs. Scheduled": ["", "", ""],
-            "Total Paid": ["8.0", "8.0", "8.0"],
-            "Regular": ["", "", ""],
-            "Unpaid Breaks": ["", "", ""],
-            "Overtime": ["", "", ""],
-            "Est. Wages": ["", "", ""],
-            "Cash Tips": ["", "", ""],
-            "Issues": ["", "", ""],
-            "No Show Reason": ["", "", ""],
-            "Employee Note": ["", "", ""],
-            "Manager Note": ["", "", ""],
-        }
-    )
+def df_1(csv_1_path: Path) -> pd.DataFrame:
+    return pd.read_csv(csv_1_path, header=3)
+
+
+@fixture
+def df_2(csv_2_path: Path) -> pd.DataFrame:
+    return pd.read_csv(csv_2_path, header=3)
+
+
+# Tests
+@param("csv_path", [csv_1_path, csv_2_path])
+def test_read_file(csv_path: Path, request):
+    csv_path = request.getfixturevalue(csv_path.__name__)
+    assert csv_path.exists()
+    df = read_file(csv_path)
+    assert len(df["Name"]) > 0
+
+
+@param("csv_path", [csv_1_path, csv_2_path])
+def test_get_payroll_period(csv_path: Path, request):
+    csv_path = request.getfixturevalue(csv_path.__name__)
+    assert csv_path.exists()
+    p1, p2 = get_payroll_period(csv_path)
+    assert p1 < p2
+    assert p1.year == p2.year
+
+
+@param("df", [df_1, df_2])
+def test_clean_blanks(df: pd.DataFrame, request):
+    df = request.getfixturevalue(df.__name__)
+    length_1 = len(df["Name"])
+    assert length_1 > 0
+    df = clean_blanks(df)
+    length_2 = len(df["Name"])
+    assert length_2 < length_1
+
+
+@param("df", [df_1, df_2])
+def test_clean_excess_headers(df: pd.DataFrame, request):
+    df = request.getfixturevalue(df.__name__)
+    length_1 = len(df["Name"])
+    assert length_1 > 0
+    df = clean_excess_headers(df)
+    length_2 = len(df["Name"])
+    assert length_2 < length_1
+
+
+@param("df", [df_1, df_2])
+def test_clean_empty_shifts(df: pd.DataFrame, request):
+    df = request.getfixturevalue(df.__name__)
+    length_1 = len(df["Name"])
+    assert length_1 > 0
+    df = clean_empty_shifts(df)
+    length_2 = len(df["Name"])
+    # Unlike other tests, I cannot assume there will always be empty shifts.
+    assert length_2 <= length_1
+
+
+@param("df", [df_1, df_2])
+def test_clean_types(df: pd.DataFrame, request):
+    df = request.getfixturevalue(df.__name__)
+    df = clean_blanks(df)
+    df = clean_excess_headers(df)
+    df = clean_empty_shifts(df)
+    df = clean_types(df)
+    assert df["Name"].dtype == "str"
+    assert df["Payroll ID"].dtype == "int"
+    assert df["Clock in datetime"].dtype == "datetime64[ns]"
+    assert df["Clock out datetime"].dtype == "datetime64[ns]"
+    assert df["Break start"].dtype == "datetime64[ns]"
+    assert df["Break end"].dtype == "datetime64[ns]"
+    assert df["Role"].dtype == "str"
+    assert df["Wage"].dtype == "int"
+    assert df["Scheduled"].dtype == "int"
+    assert df["Issues"].dtype == "str"
+    assert df["Employee Note"].dtype == "str"
+    assert df["Manager Note"].dtype == "str"
+    assert df["Break paid"].dtype == "bool"
